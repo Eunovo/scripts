54c55d90aa007a4fc3b8bf63920cb500d2ff60de
Eunovo <eunovo9@gmail.com>
2024-10-04 08:44:09 +0100

    Update to latest desc spec

--- silent_payment/generate_sp_descriptors.py
+++ silent_payment/generate_sp_descriptors.py
@@ -3,8 +3,21 @@ Useful for generating SP various descriptors for testing
 """
 from bip32 import bip32
 from bech32 import bech32
+import argparse
 
-def generate_sp_descriptors(seed: str, paths: list[str]):
+VERSION_HEX = "00"
+VCH_FINGERPRINT = "00000000"
+F_ALLOW_LABELS = "01"
+SP_HRPS = {
+  "mainnet": ("spprv", "sppub"),
+  "regtest": ("sprtprv", "sprtpub")
+}
+EXT_HRPS = {
+  "mainnet": ("xprv", "xpub"),
+  "regtest": ("tprv", "tpub")
+}
+
+def generate_sp_descriptors(seed: str, paths: list[str], network: str):
   master_key = bip32.GenerateMasterKey(bytes.fromhex(seed))
   keys = []
   for path in paths:
@@ -16,8 +29,8 @@ def generate_sp_descriptors(seed: str, paths: list[str]):
       depth += 1
       assert(ext_key)
 
-    ext_key_str = bip32.ToExtKeyString('xprv', 0, b"\x00\x00\x00\x00", b"\x00\x00\x00\x00", master_key.chain_code, master_key.parent_private_key)
-    ext_pubkey_str = bip32.ToExtKeyString('xpub', 0, b"\x00\x00\x00\x00", b"\x00\x00\x00\x00", master_key.chain_code, bip32.ToPublicKey(master_key.parent_private_key))
+    ext_key_str = bip32.ToExtKeyString(EXT_HRPS[network][0], 0, b"\x00\x00\x00\x00", b"\x00\x00\x00\x00", master_key.chain_code, master_key.parent_private_key)
+    ext_pubkey_str = bip32.ToExtKeyString(EXT_HRPS[network][1], 0, b"\x00\x00\x00\x00", b"\x00\x00\x00\x00", master_key.chain_code, bip32.ToPublicKey(master_key.parent_private_key))
     privkey = ext_key.parent_private_key.hex()
     pubkey = bip32.ToPublicKey(ext_key.parent_private_key).hex()
     keys.append((ext_key_str+path, ext_pubkey_str+path, privkey, pubkey))
@@ -26,8 +39,8 @@ def generate_sp_descriptors(seed: str, paths: list[str]):
     first_key = key[2]
     for other in keys:
       for second_key in [other[2], other[3]]:
-        raw_sp_key = "000000000000000000" + first_key + ("00"+second_key if len(second_key) == 64 else second_key)
-        bech32_sp_key = bech32.encode("spprv" if len(second_key) == 64 else "sppub", bytes.fromhex(raw_sp_key))
+        raw_sp_key = VERSION_HEX + VCH_FINGERPRINT + F_ALLOW_LABELS + first_key + ("00"+second_key if len(second_key) == 64 else second_key)
+        bech32_sp_key = bech32.encode(SP_HRPS[network][0] if len(second_key) == 64 else SP_HRPS[network][1], bytes.fromhex(raw_sp_key))
         print(f"sp({key[0]},{other[0 if len(second_key) == 64 else 1]})\nsp({bech32_sp_key})\n{first_key} {second_key}\n")
 
 SEED = "000102030405060708090a0b0c0d0e0f"
@@ -41,4 +54,7 @@ PATHS = [
 ]
 
 if __name__ == "__main__":
-  generate_sp_descriptors(SEED, PATHS)
+  parser = argparse.ArgumentParser(prog='Silent Payment Descriptor Generator', description='Generate Silent Payment Descriptors for testing')
+  parser.add_argument('-n', '--network', choices=['mainnet', 'regtest'], default='mainnet')
+  args = parser.parse_args()
+  generate_sp_descriptors(SEED, PATHS, args.network)
